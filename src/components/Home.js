bffcdb81a9374c44fe089de7c02438f5388b30d4
Marco Arboleda <arboleda.marco@gmail.com>
2020-09-29 17:37:10 -0400

    Add text for Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import React from 'react';
 const home = () => {
     return (
         <div>
-            <h1>Hello from Home</h1>
+            <p>Struggling with Weight? Want to live a healthier lifestyle?</p>
+            <p>Health is Wealth offers meals prepared by professional nutritionists and dieticians to help you reach you fitness goals.</p>
+            <p>Order one of our specially prepared meals or book a consultation with one of our nutritionists today!</p> 
         </div>
     )
 }
