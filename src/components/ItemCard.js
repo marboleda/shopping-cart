f38fe0f840f512aee2f1cddc7e087ba74175b45f
Marco Arboleda <arboleda.marco@gmail.com>
2020-09-29 17:39:26 -0400

    Set up ItemCard component

---
+++ src/components/ItemCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const itemCard = () => {
+    return (
+        <div className="item">
+            <img src=''></img>
+            <h3></h3>
+        </div>
+    )
+}
+
+export default itemCard;
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ItemCard from './ItemCard'
 
 const shop = () => {
     return (
