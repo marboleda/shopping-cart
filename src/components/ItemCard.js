216273082207c7e890e76f016a836a115b3684f0
Marco Arboleda <arboleda.marco@gmail.com>
2020-10-03 11:53:08 -0400

    Track number of each item

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ItemCard.css';
 
-const itemCard = (props) => {
+const ItemCard = (props) => {
+    const [numOfItem, setNumOfItem] = useState(0);
+
     const { itemId, itemImage, itemDesc, addItems } = props;
+
+    const updateNumOfItem = (newNum) => {
+        setNumOfItem(newNum);
+    }
 
     return (
         <div className="item">
             <img src={itemImage}></img>
             <h3>{itemDesc}</h3>
+            <input 
+                type="number" 
+                className='num-of-items'
+                value={numOfItem}
+                onChange={(e) => updateNumOfItem(Number(e.target.value))}/>
         </div>
     )
 }
 
-export default itemCard;
+export default ItemCard;
