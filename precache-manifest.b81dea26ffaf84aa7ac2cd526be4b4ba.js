09b7fa886446c0fc8739637be25bf9f22c0f6791
Marco Arboleda <arboleda.marco@gmail.com>
2020-10-03 14:29:25 -0400

    Updates

---
+++ precache-manifest.b81dea26ffaf84aa7ac2cd526be4b4ba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb3593fc65f75b3864f641411e808cc",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "6f498c14faf443e84019",
+    "url": "/shopping-cart/static/css/main.0cbac088.chunk.css"
+  },
+  {
+    "revision": "eab4593371f98cc0dc83",
+    "url": "/shopping-cart/static/js/2.3063148e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart/static/js/2.3063148e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f498c14faf443e84019",
+    "url": "/shopping-cart/static/js/main.812f9c22.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  },
+  {
+    "revision": "baeab78ebffee28efe1d0fbb75cc34ba",
+    "url": "/shopping-cart/static/media/chicken-and-ptitim-salad.baeab78e.png"
+  },
+  {
+    "revision": "a3eac73768d27c7663a4bbe18042566a",
+    "url": "/shopping-cart/static/media/eggplant-and-potato-puree.a3eac737.png"
+  },
+  {
+    "revision": "10b572381d487d909f1a9fb5e96350aa",
+    "url": "/shopping-cart/static/media/sweet-chili-salmon-and-brown-rice.10b57238.png"
+  },
+  {
+    "revision": "47238c347e87e06e118e0c38b108c645",
+    "url": "/shopping-cart/static/media/tomato-whole-wheat-spaghetti.47238c34.png"
+  }
+]);
